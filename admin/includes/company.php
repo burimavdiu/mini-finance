4a908ce5316f09fbf08a142ae6dbdb8461a351e8
Burim Avdiu <burimavdiu@yahoo.com>
2018-01-03 15:25:21 +0100

    Shtimi i funksioneve per konfigurime

--- admin/includes/company.php
+++ admin/includes/company.php
@@ -1,22 +1,45 @@
 
 	<?php
+		
+		//$company_res=findComapany();
+		//$user=mysqli_fetch_assoc($company_res);
+		/*$firstname=$user['firstname'];
+		$lastname=$user['lastname'];
+		$dep_id=$user['dep_id'];
+		$dep_name=$user['dep_name'];
+		
+		$phone=$user['phone'];
+		$email=$user['email'];
+		$username=$user['username'];
+		$password=$user['password'];
+		*/
 		$mesazhi="";
+		if(isset($_POST['updateCompany'])){
+			
+			$mesazhi=updateUser($_POST['user_id'],$_POST['firstname'],$_POST['lastname'],
+			$_POST['departments'],$_POST['password'],$_POST['email'],
+			$_POST['username'],$_POST['phone'],$registrar);
+		}
 		
 	?>
 	<!-- card-register forma e regjistrimit--> 
     <div class="card  mx-auto mt-30 ">
-      <div class="card-header h4">Regjistrimi i Përdoruesit</div>
+      <div class="card-header h4">Konfigurimi Kompania</div>
       <div class="card-body mx-5">
-        <form method="post" id="adduser">		  
+        <form method="post" id="updateuser">		  
+		   <input name="user_id" value="<?php if(!empty($user_id)) echo $user_id;?>"
+				class="form-control" id="user_id" type="hidden">
 		  <div class="form-group">
             <div class="form-row">
               <div class="col-md-6">
                 <label class="h6" for="firstname">Emri: </label>
-                <input name="firstname" class="form-control" id="firstname" type="text" aria-describedby="nameHelp">
+                <input name="firstname" value="<?php if(!empty($firstname)) echo $firstname;?>"
+				class="form-control" id="firstname" type="text">
               </div>
               <div class="col-md-6">
                 <label class="h6" for="lastname">Mbiemri: </label>
-                <input name="lastname" class="form-control" id="lastname" type="text" aria-describedby="nameHelp">
+                <input name="lastname" value="<?php if(!empty($lastname)) echo $lastname;?>" 
+				class="form-control" id="lastname" type="text" aria-describedby="nameHelp">
               </div>
             </div>
           </div>
@@ -24,35 +47,43 @@
             <label for="department">Departamenti :</label>
             <?php
                 echo '<select name="departments" class="form-control"  id="department">';
+				echo "<option value='".$dep_id."'> ". $dep_name . "</option>";
 				$departments=findDepartments();
 				
                 while($dep=mysqli_fetch_array($departments)){
-                    echo "<option value='".$dep['dep_id']."'> ".$dep['dep_name']. "</option>";
+                    if($dep_id!=$dep['dep_id']){
+						echo "<option value='".$dep['dep_id']."'> ".$dep['dep_name']. "</option>";
+					}
                 }
                 echo '</select>';
             ?>
           </div>
           <div class="form-group">
             <label class="h6" for="email">Email :</label>
-            <input name="email" class="form-control" id="email" type="text" aria-describedby="emailHelp" >
+            <input name="email" value="<?php if(!empty($email)) echo $email;?>" 
+			class="form-control" id="email" type="text" aria-describedby="emailHelp" >
           </div>
 		  <div class="form-group">
             <label class="h6" for="phone">Telefoni :</label>
-            <input name="phone" class="form-control" id="phone" type="text" aria-describedby="telefoniHelp" >
+            <input name="phone" value="<?php if(!empty($phone)) echo $phone;?>"
+			class="form-control" id="phone" type="text" aria-describedby="telefoniHelp" >
           </div>
 		   <div class="form-group">
             <label class="h6" for="username">Përdoruesi :</label>
-            <input name="username" class="form-control" id="username" type="text" aria-describedby="perdoruesiHelp" >
+            <input name="username" value="<?php if(!empty($username)) echo $username;?>" 
+			class="form-control" id="username" type="text" aria-describedby="perdoruesiHelp" >
           </div>
           <div class="form-group">
             <div class="form-row">
               <div class="col-md-6">
                 <label class="h6" for="password">Fjalëkalimi :</label>
-                <input name="password" class="form-control" id="password" type="password">
+                <input name="password" value="<?php if(!empty($password)) echo $password;?>"
+				class="form-control" id="password" type="password">
               </div>
               <div class="col-md-6">
                 <label class="h6" for="confirmPassword">Konfirmo Fjalëkalimin :</label>
-                <input class="form-control" id="confirmPassword" type="password">
+                <input class="form-control" value="<?php if(!empty($password)) echo $password;?>"
+				id="confirmPassword" type="password">
               </div>
             </div>
             </div>
@@ -61,116 +92,12 @@
 			  <span id="message"></span>
 			</div>
 			
-		  <input name="addUser" type="submit" class="btn btn-primary btn-block" value="Regjistro">
+		  <input name="updateUser" type="submit" class="btn btn-primary btn-block" value="Modifiko">
         </form>
       </div>
     </div>
-<div class="card mb-3">
-        <div class="card-header">
-          <i class="fa fa-table"></i> Lista e Kompanive</div>
-        <div class="card-body">
-		  <?php if (isset($_SESSION['mesazhi'])){?>		
-			  <div class="alert alert-success malert">
-				  <?php echo $_SESSION['mesazhi'];
-					unset($_SESSION['mesazhi']);
-				  ?>
-			  </div>
-		  <?php }
-			if(isset($_GET['source'])){
-				$user_id = $_GET['user_id'];
-				delteUser($user_id);
-			}
-		  ?>
-          <div class="table-responsive">
-            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-              <thead>
-                <tr>
-                  <th>Id</th>
-                  <th>Emri</th>
-                  <th>Adresa 1</th>
-                  <th>Adresa 2</th>
-                  <th>Telefoni</th>
-                  <th>Mobili</th>
-                  <th>Faksi</th>
-                  <th>Nr. i biznesit</th>
-                  <th>TVSH</th>
-                  <th>Nr. fiskal</th>
-                  <th>LLogaria bankare 1</th>
-                  <th>LLogaria bankare 2</th>
-                  <th>Emaili i kompanisë</th>
-                  <th>Webi i kompanisë</th>
-                  <th>Edit</th>
-				  <th>Delete</th>
-                </tr>
-              </thead>
-              <tfoot>
-                <tr>
-                  <th>Id</th>
-                  <th>Emri</th>
-                  <th>Adresa 1</th>
-                  <th>Adresa 2</th>
-                  <th>Telefoni</th>
-                  <th>Mobili</th>
-                  <th>Faksi</th>
-                  <th>Nr. i biznesit</th>
-                  <th>TVSH</th>
-                  <th>Nr. fiskal</th>
-                  <th>LLogaria bankare 1</th>
-                  <th>LLogaria bankare 2</th>
-                  <th>Emaili i kompanisë</th>
-                  <th>Webi i kompanisë</th>
-                  <th>Edit</th>
-				  <th>Delete</th>
-                </tr>
-              </tfoot>
-              <tbody>
-				<?php
-				$companies=findCompanies();
-				while($company=mysqli_fetch_array($companies)) {
-				$company_id=$company['company_id'];
-				echo "<tr>";
-				echo "<td>".  $company_id . "</td>";
-				echo "<td>".  $company['company_name'] . "</td>";
-				echo "<td>".  $company['address_1'] . "</td>";
-				echo "<td>".  $company['address_2'] . "</td>";
-				echo "<td>".  $company['tel_no'] . "</td>";
-				echo "<td>".  $company['mobile_no'] . "</td>";
-				echo "<td>".  $company['fax_no'] . "</td>";
-				echo "<td>".  $company['business_no'] . "</td>";
-				echo "<td>".  $company['vat'] . "</td>";
-				echo "<td>".  $company['fiscal_no'] . "</td>";
-				echo "<td>".  $company['bank_acc_1'] . "</td>";
-				echo "<td>".  $company['bank_acc_2'] . "</td>";
-				echo "<td>".  $company['company_email'] . "</td>";
-				echo "<td>".  $company['company_web'] . "</td>";
-				echo "<td><a href='companies.php?source=edit_company&company_id=$company_id'>Edit</a></td>";
-				echo "<td><a href='companies.php?company_id=$company_id' data-toggle='modal' data-target='#confirm-delete' class='confirm-delete'>Delete</a></td>";
-				echo "</tr>";
-				}
-			  ?>
-              </tbody>
-            </table>
-          </div>
-        </div>
-        <div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						Fshirja e kompanisë
-					</div>
-					<div class="modal-body">
-						A dëshironi të fshini kompaninë?
-					</div>
-					<div class="modal-footer">
-					  <a class='btn btn-danger' 
-					  href='companies.php?source=delete_company&company_id=<?php echo $company_id; ?>'> Po</a>
-					  <a href="#" data-dismiss="modal" aria-hidden="true" class="btn secondary">Jo</a>
-					</div>
-				</div>
-			</div>
-		</div>
 	<script>
-	$("#adduser").submit(function () {
+	$("#updateuser").submit(function () {
        
 		
 		kontrollues=false;
@@ -199,7 +126,7 @@
             message+="Ju lutem plotësoni perdoruesin<br>";
             kontrollues=true;
         }
-		if ($("#password").val() == "" || 
+	if ($("#password").val() == "" || 
 		($("#password").val()!=$("#confirmPassword").val()) ) {
             message+="Ju lutem plotësoni | verifikoni fjalekalimin<br>";
             kontrollues=true;
@@ -214,7 +141,4 @@
 		}
 		
     });
-    setTimeout(function(){
-        $('.malert').addClass('d-none');
-    },8000);
   </script>
